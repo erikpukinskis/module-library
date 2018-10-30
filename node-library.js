e20eda9d9cd9ce93d33ed72e70a0c42737a106d6
Erik Pukinskis <erik@snowedin.net>
2018-10-30 15:33:03 -0700

    dealias on singletons

--- node-library.js
+++ node-library.js
@@ -254,6 +254,7 @@ function libraryFactory(alternateRequire) {
 
   boundFunc = newLibrary.define.bind(newLibrary)
   boundFunc.define = boundFunc
+  boundFunc.dealias = newLibrary.dealias.bind(newLibrary)
   boundFunc.using = newLibrary.using.bind(newLibrary)
   boundFunc.run = boundFunc.using
   boundFunc.export = newLibrary.export.bind(newLibrary)
